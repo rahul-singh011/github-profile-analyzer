73b4e90d46a681888a96270cd47b5953ab96e5f4
Rahul Singh <rahul.singh01711@gmail.com>
2026-06-11 13:02:04 +0530

    DB connection pool and utility classes

---
+++ src/utils/ApiError.js
@@ -0,0 +1,11 @@
+
+class ApiError extends Error {
+    constructor(statusCode, message = "Something went wrong") {
+      super(message);
+      this.statusCode = statusCode;
+      this.success = false;
+      this.data = null;
+    }
+  }
+   
+  module.exports = ApiError;
